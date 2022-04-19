03bd72fe262ceeb31869b3bcb5d4b7658588f784
emilyelhy <emilyelhy@yahoo.com>
2022-04-19 19:17:33 +0900

    Welcome page text input done

--- src/Component/WelcomePage.js
+++ src/Component/WelcomePage.js
@@ -6,46 +6,93 @@
  * @flow strict-local
  */
 
- import React from 'react';
- import {
-   SafeAreaView,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- import Clipboard from '@react-native-community/clipboard';
- import Entypo from 'react-native-vector-icons/Entypo';
- import FlashMessage from "react-native-flash-message";
- import { showMessage } from "react-native-flash-message";
- 
- export default function WelcomePage()  {
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const backgroundStyle = {
-     flex: 1,
-     backgroundColor: "#DAE2E1",
-   };
+import React, { useRef } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  useColorScheme,
+  View,
+  TextInput,
+  Button,
+} from 'react-native';
+import Clipboard from '@react-native-community/clipboard';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FlashMessage from "react-native-flash-message";
+import { showMessage } from "react-native-flash-message";
+
+export default function WelcomePage() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: "#DAE2E1",
+  };
+
+  const connectData = useRef({ ipAddress: "", port: 0 });
+  const handleIPAddress = (value) => { connectData.current.ipAddress = value; };
+  const handlePort = (value) => { connectData.current.port = value; };
+
+  const submitConnection = async () => {
+    if (!connectData.current.ipAddress) showMessage({ message: "Please enter IP Address first" });
+    if (connectData.current.port === 0) showMessage({ message: "Please enter port first" });
+    console.log("IPAddress: " + connectData.current.ipAddress);
+    console.log("Port: " + connectData.current.port);
+  }
 
   const CopyLink = () => {
     Clipboard.setString("https://www.pplus.com/windows/install");
     showMessage({ message: "Copied to clipboard" });
   }
- 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <View style={{ justifyContent: "space-evenly", flex: 1 }}>
-         <FlashMessage position="bottom" />
-         <Text style={{ fontSize: 40, fontWeight: "600", color: "#7B8D93", textAlign: "center" }}>Welcome{"\n"}to PPlus</Text>
-         <Text style={{ fontSize: 20, fontWeight: "400", color: "#7B8D93", textAlign: "center" }}>Please enter the following{"\n"}information to finish initial pairing</Text>
-         <View>
-           <Text style={{ fontSize: 15, fontWeight: "400", color: "#7B8D93", textAlign: "center" }}>Not yet install PPlus PC host?</Text>
-           <Entypo.Button name="share" backgroundColor="#DAE2E1" justifyContent="center" onPress={CopyLink} iconStyle={{ color: "#7B8D93", marginRight: 3, marginTop: 0 }}>
-             <Text style={{ color: '#7B8D93', fontSize: 15, textDecorationLine: 'underline' }}>
-               SHARE INSTALLATION LINK
-             </Text>
-           </Entypo.Button>
-         </View>
-       </View>
-     </SafeAreaView>
-   );
- };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View style={{ justifyContent: "space-evenly", flex: 1 }}>
+        <FlashMessage position="bottom" />
+        <Text style={{ fontSize: 40, fontWeight: "600", color: "#7B8D93", textAlign: "center" }}>
+          Welcome{"\n"}to PPlus
+        </Text>
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: "400", color: "#7B8D93", textAlign: "center" }}>
+            Please enter the following{"\n"}information to finish initial pairing
+          </Text>
+          <View style={{ justifyContent: "center", flexDirection: "row" }}>
+            <TextInput
+              placeholder="IP Address"
+              keyboardType="numeric"
+              style={{ borderColor: "#999999", borderWidth: 1, width: '55%', height: 40, marginTop: 10, marginRight: 10 }}
+              onChangeText={(value) => handleIPAddress(value)}
+            />
+            <TextInput
+              placeholder="Port"
+              keyboardType="numeric"
+              style={{ borderColor: "#999999", borderWidth: 1, width: '25%', height: 40, marginTop: 10 }}
+              onChangeText={(value) => handlePort(value)}
+            />
+          </View>
+          <View style={{ marginTop: 10, width: '30%', alignSelf: "center" }}>
+            <Button
+              title="CONNECT"
+              color="#989da5"
+              onPress={submitConnection}
+            />
+          </View>
+        </View>
+        <View>
+          <Text style={{ fontSize: 15, fontWeight: "400", color: "#7B8D93", textAlign: "center" }}>
+            Not yet install PPlus PC host?
+          </Text>
+          <Entypo.Button
+            name="share"
+            backgroundColor="#DAE2E1"
+            justifyContent="center"
+            onPress={CopyLink}
+            iconStyle={{ color: "#7B8D93", marginRight: 3, marginTop: 0 }}>
+            <Text style={{ color: '#7B8D93', fontSize: 15, textDecorationLine: "underline" }}>
+              SHARE INSTALLATION LINK
+            </Text>
+          </Entypo.Button>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
